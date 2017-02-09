b4fc131de197711d2620295a21eddca70b1eabdd
Junseong Yang <Junseong Yang>
2017-02-09 09:24:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/junseongdanielyang/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.junseongdanielyang.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        forceError();
+    }
+
+
+        public void forceError() {
+            if(true) {
+                throw new Error("Whoops");
+
+        }
+
+    }
+}
